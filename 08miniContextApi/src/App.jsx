442281417bed134351b330a9e40b5a614e300f6a
Paresh Kumar Pradhan <pareshkupradhan789@gmail.com>
2024-04-11 22:41:49 +0530

    context api

---
+++ 08miniContextApi/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import UserContextProvider from './context/UserContext'
+
+function App() {
+ 
+
+  return (
+    <UserContextProvider>
+      <h1>Paresh Kuamr Pradhan</h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
